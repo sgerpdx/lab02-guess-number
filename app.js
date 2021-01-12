c2d6ee3ecacd051e26481b58bf3d0792e1ac74c5
SamuelHGerber <henrylightfoot@gmail.com>
2021-01-12 13:44:01 -0800

    added button and created and validated DOM elments

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 // import functions and grab DOM elements
 
+
+const guessCounter = document.getElementById('counter');
+const numberGuess = document.getElementById('number-guess');
+const guessButton = document.getElementById('guess-button');
+
+console.log(guessButton);
+console.log(numberGuess);
+console.log(guessCounter);
+
+console.log('wisconsin');
+
+
+
+
+
+
+
+
+
 // initialize state
 
 // set event listeners to update state and DOM
