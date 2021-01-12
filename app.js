03280ed6d36c3d878a26bfa950305b564bececf3
SamuelHGerber <henrylightfoot@gmail.com>
2021-01-12 15:29:26 -0800

    decrement of remainingGuesses works

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// import functions and grab DOM elements
+
 
 import { guessEval } from './domUtils.js';
 
@@ -8,28 +8,27 @@ const guess = document.getElementById('number-guess');
 const userFeedback = document.getElementById('feedback-message');
 const guessButton = document.getElementById('guess-button');
 
-console.log('whaaatup');
-
 
 let remainingGuesses = 4;
 let randomNumber = Math.ceil(Math.random() * 20);
 console.log(randomNumber);
 
+
 guessButton.addEventListener('click', () => {
 
+    remainingGuesses = (remainingGuesses - 1);
 
     const guessInput = guess.valueAsNumber;
 
-
     if (guessEval(guessInput, randomNumber) > 0) {
 
-        remainingGuesses = remainingGuesses--;
+
         userFeedback.textContent = 'Too high - try again!';
         guessTracker.textContent = `You have ${remainingGuesses} chances left to guess.`;
 
     } else if (guessEval(guessInput, randomNumber) < 0) {
 
-        remainingGuesses = remainingGuesses--;
+
         userFeedback.textContent = 'Too low - try again!';
         guessTracker.textContent = `You have ${remainingGuesses} chances left to guess.`;
 
@@ -41,9 +40,8 @@ guessButton.addEventListener('click', () => {
 
 
 
-    console.log(userFeedback.textContent);
-    console.log(guessTracker.textContent);
-    console.log(remainingGuesses);
+
+
 
 
 });
