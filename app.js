60fdd9ec11174cdb6d16b857693dcfee654a711a
SamuelHGerber <henrylightfoot@gmail.com>
2021-01-12 21:36:38 -0800

    functions and guess-tracking work; loop still a work in progress to end game at 0 guesses remaining

--- app.js
+++ app.js
@@ -8,26 +8,25 @@ const guess = document.getElementById('number-guess');
 const userFeedback = document.getElementById('feedback-message');
 const guessButton = document.getElementById('guess-button');
 
+// const reLaunch = document.getElementById('play-again');
+
 
 let remainingGuesses = 4;
 let randomNumber = Math.ceil(Math.random() * 20);
-console.log(randomNumber);
 
 
 guessButton.addEventListener('click', () => {
 
     remainingGuesses = (remainingGuesses - 1);
-
     const guessInput = guess.valueAsNumber;
 
-    if (guessEval(guessInput, randomNumber) > 0) {
 
+    if (guessEval(guessInput, randomNumber) > 0) {
 
         userFeedback.textContent = 'Too high - try again!';
         guessTracker.textContent = `You have ${remainingGuesses} chances left to guess.`;
 
     } else if (guessEval(guessInput, randomNumber) < 0) {
-
 
         userFeedback.textContent = 'Too low - try again!';
         guessTracker.textContent = `You have ${remainingGuesses} chances left to guess.`;
@@ -36,20 +35,16 @@ guessButton.addEventListener('click', () => {
 
         userFeedback.textContent = 'Congratulations! You guessed correctly!';
         return;
+
     }
 
-
-
-
-
+    // Now I just need to put in the limit on guesses (and correct guesses) so that after 4 or after a correct guess, the function is exited.
+    // While Loop: has been attempted, will be attempted again.
+    // Also add functionality for the button to play again after the game is complete.
 
 
 });
 
 
-
-
-
-// set event listeners to update state and DOM
 
 
